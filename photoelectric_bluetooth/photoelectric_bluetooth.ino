6646631d5e3970681d08c865e409c9fcef832512
ChunmengYang <277048985@qq.com>
2020-06-12 17:54:46 +0800

    init

--- photoelectric_bluetooth/photoelectric_bluetooth.ino
+++ photoelectric_bluetooth/photoelectric_bluetooth.ino
@@ -2,6 +2,7 @@ int inputPin = 3;
 
 uint8_t prev = 1;
 int pre_time = 0;
+int pre_pin_time = 0;
 
 void setup() {  
   // 3号数字口设置为输入状态
@@ -20,12 +21,18 @@ void setup() {
 }
 
 void loop() {
-  int sensorValue = digitalRead(inputPin);
+  int pin_time = millis();
+  // 两个连续的读数间隔必须大于500毫秒
+  if((pin_time - pre_pin_time) > 1000) {
+    Serial.println("0");
+    pre_pin_time = pin_time;
+  }
   
+  int sensorValue = digitalRead(inputPin);
   if(sensorValue == 0 && sensorValue != prev) {
     int time = millis();
     // 两个连续的读数间隔必须大于500毫秒
-    if((time - pre_time) > 500) {
+    if((time - pre_time) > 1000) {
       pre_time = time;
       Serial.println("1");
     }
